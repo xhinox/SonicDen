2229c8ae4bd8f7f21e9f2069a272b4866469be40
xhinox <augusto.ferriz@gmail.com>
2019-10-06 01:59:19 -0700

    Updates

---
+++ precache-manifest.1c352d92d8755658d66d877d55bc2210.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a10b8fe3e5018574f81ba533eb533b28",
+    "url": "/sonicden/index.html"
+  },
+  {
+    "revision": "9c8b25a2ca2987c7c4ba",
+    "url": "/sonicden/static/css/main.05ff2980.chunk.css"
+  },
+  {
+    "revision": "ee07a8bbf64a0a7000ae",
+    "url": "/sonicden/static/js/2.97bdd0f3.chunk.js"
+  },
+  {
+    "revision": "9c8b25a2ca2987c7c4ba",
+    "url": "/sonicden/static/js/main.3d605153.chunk.js"
+  },
+  {
+    "revision": "5f42f6486e375e14a64c",
+    "url": "/sonicden/static/js/runtime~main.4a581c56.js"
+  },
+  {
+    "revision": "37f5e07d4f99e10dfb0b58c8f11b9cc1",
+    "url": "/sonicden/static/media/f01.37f5e07d.jpg"
+  },
+  {
+    "revision": "87fbb63180cbce2381d97c5365f58803",
+    "url": "/sonicden/static/media/f01x.87fbb631.jpg"
+  },
+  {
+    "revision": "4b3914c97fc491ee14591aa82b2b0737",
+    "url": "/sonicden/static/media/f02.4b3914c9.jpg"
+  },
+  {
+    "revision": "baa2837f63dbd2a7f84ff94680133eaf",
+    "url": "/sonicden/static/media/f02x.baa2837f.jpg"
+  },
+  {
+    "revision": "d6da5378fe55711fb60a6c16159a8209",
+    "url": "/sonicden/static/media/f03.d6da5378.jpg"
+  },
+  {
+    "revision": "60783191920cf447aa2f460a2c295a73",
+    "url": "/sonicden/static/media/f03x.60783191.jpg"
+  },
+  {
+    "revision": "4d460eba7ba86f6bf02371c443d97cb1",
+    "url": "/sonicden/static/media/usa-flag.4d460eba.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sonicden/precache-manifest.8ebcf4d63ba322395c36bb054aab3880.js"
+  "/sonicden/precache-manifest.1c352d92d8755658d66d877d55bc2210.js"
 );
 
 self.addEventListener('message', (event) => {
